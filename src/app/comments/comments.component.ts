f2d2853ff393dcaffafcf2007d86a16c387b4df6
katlego.rampedi <katlego.rampedi@Ekurhuleni.gov.za>
2023-07-18 13:47:54 +0200

    fixed the toolbar

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./comments.component.scss']
 })
 export class CommentsComponent {
-
+  icons = [
+    { class: 'fa-solid fa-face-smile fa-xl', name: 'Face Smile' },
+    { class: 'fa-solid fa-paperclip fa-xl', name: 'Attach' },
+    { class: 'input', name: 'Input' },
+    { class: 'fa-solid fa-microphone fa-xl', name: 'Mic' }
+  ];
+  
 }
--- src/app/side-icons/side-icons.component.ts
+++ src/app/side-icons/side-icons.component.ts
@@ -10,7 +10,7 @@ export class SideIconsComponent {
 notifications = [
   { iconClass: 'fa-solid fa-bell fa-xl', text: 'Notification' },
   { iconClass: 'fa-solid fa-shield-halved fa-xl', text: 'Security' },
-  { iconClass: 'fa-solid fa-certificate fa-xl', text: 'Theme' },
+  { iconClass: 'fa-solid fa-palette fa-xl', text: 'Theme' },
   { iconClass: 'fa-regular fa-image fa-xl', text: 'Chat Wallpaper' },
   { iconClass: 'fa-solid fa-ban fa-rotate-90 fa-xl', text: 'Blocked' },
   { iconClass: 'fa-solid fa-desktop fa-xl', text: 'Desktop Settings' },
